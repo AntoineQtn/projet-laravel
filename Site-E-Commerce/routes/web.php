f6391e549044dd94db24b63fffd598e25aa15dd9
Antoine QUINTIN <antoine.quintin@le-campus-numerique.fr>
2025-07-22 12:16:08 +0200

    feat/ routes creatoin and text printing

--- Site-E-Commerce/routes/web.php
+++ Site-E-Commerce/routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Homepage');
+});
+Route::get('/product', function () {
+    echo 'Hello Product';
+});
+Route::get('/product/{id}', function () {
+    echo 'Hello ProductCard';
+});
+Route::get('/cart', function () {
+    echo 'Hello Cart';
 });
